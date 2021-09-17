51fecdd445d567463b0da0f2c5d3690272ab1571
joseflekardal <joseflekardal@gmail.com>
2021-09-18 01:09:59 +0200

    feat: first draft

---
+++ netlify/functions/pco/pco.js
@@ -0,0 +1,73 @@
+const axios = require("axios");
+
+const httpClient = axios.create({
+  baseURL: "https://api.planningcenteronline.com/services/v2",
+  auth: {
+    username: process.env.PCO_ID,
+    password: process.env.PCO_SECRET
+  }
+});
+
+httpClient.interceptors.response.use(response => response.data);
+
+const handler = async function(event, context) {
+  try {
+    const plansResponse = await httpClient.get(
+      `/service_types/${event.queryStringParameters.serviceTypeId}/plans`,
+      {
+        params: {
+          filter: "future",
+          per_page: 1
+        }
+      }
+    );
+
+    const teams = await httpClient.get(
+      `/service_types/${event.queryStringParameters.serviceTypeId}/teams`,
+      {
+        params: {
+          include: "team_positions"
+        }
+      }
+    );
+    const teamMembers = await httpClient.get(
+      plansResponse.data[0].links.self + "/team_members"
+    );
+
+    const res = teams.data
+      .filter(team => team.attributes.name.includes("Worship"))
+      .map(team => {
+        return {
+          id: team.id,
+          name: team.attributes.name,
+          positions: team.relationships.team_positions.data
+            .map(tp => {
+              const position = teams.included.find(pos => pos.id === tp.id);
+              const people = teamMembers.data.filter(
+                tm =>
+                  tm.attributes.team_position_name === position.attributes.name
+              );
+              return {
+                id: position.id,
+                name: position.attributes.name,
+                people: people.map(p => ({ id: p.id, ...p.attributes }))
+              };
+            })
+            .filter(tp => tp.people.length)
+        };
+      });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res)
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: error.message })
+    };
+  }
+};
+
+module.exports = { handler };
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,49 @@
 <template>
-  <Tutorial/>
+  <main class="p-5">
+    <h1 class="text-5xl font-bold mb-10">Teams söndag</h1>
+    <section>
+      <div v-for="team in teams" class="mb-10" :key="team.id">
+        <h2>{{ team.name }}</h2>
+        <ul>
+          <li v-for="position in team.positions" :key="position.id">
+            <strong>{{ position.name }}</strong>
+            <span
+              v-for="person in position.people"
+              :class="
+                person.status === 'C' ? 'text-green-500' : 'text-yellow-500'
+              "
+              :key="person.id"
+            >
+              {{ person.name }}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-export default {}
+import axios from "axios";
+export default {
+  data() {
+    return {
+      serviceType: null,
+      positions: null,
+      teams: null
+    };
+  },
+  async mounted() {
+    const services = {
+      cityPm: "1155898",
+      cityAm: "1155896"
+    };
+
+    const pcoResponse = await axios.get(
+      `/.netlify/functions/pco?serviceTypeId=${services.cityPm}`
+    );
+
+    this.teams = pcoResponse.data;
+  }
+};
 </script>
